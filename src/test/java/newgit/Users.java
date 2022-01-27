0770b6d6cdef8deb8d871e0adff20f4385a5358e
hp <hp@DESKTOP-FFVD44P>
2022-01-27 21:19:30 +0530

    user 1 third commit

--- src/test/java/newgit/Users.java
+++ src/test/java/newgit/Users.java
@@ -10,6 +10,7 @@ public class Users {
 		System.out.println("user 2");
 		System.out.println("user 1 second commit");
 		System.out.println("user 2 try to commit without pull");
+		System.out.println("user 1 third commit");
 	}
 
 }
